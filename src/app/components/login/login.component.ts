3309d2943beb053db2d763aa7ffc4012e6f09592
Vladislav Faerman <faerman@hm.edu>
2017-04-01 23:44:02 +0200

    refactored bootstrap config, added js files for bootstrap

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,14 +2,37 @@
  * Created by vlfa on 01.04.17.
  */
 import {Component, ElementRef} from '@angular/core';
-import {User} from "../../models/user";
+import {Observable} from "rxjs";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'login-form',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 
 export class LoginComponent {
 
+  constructor(private userService: UserService) {
+  }
+
+  createUser(userName, passWord, email) {
+    let user = {
+      userName: userName,
+      passWord: passWord,
+      email: email
+    };
+    this.userService.createUser(user).subscribe(
+      data => {
+        //   console.log(data)
+        // refresh the list
+        return true;
+      },
+      error => {
+        console.error("Error create a new User!");
+        return Observable.throw(error);
+      }
+    );
+  }
 }
